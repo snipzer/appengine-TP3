7f3b51dd9fe330285f65278ac17a7c86e6ec706b
snipzer <l.arif@laposte.net>
2019-02-11 15:47:26 +0100

    Adding AuthFilter

--- src/main/java/com/snipzer/contact/service/AuthenticationService.java
+++ src/main/java/com/snipzer/contact/service/AuthenticationService.java
@@ -6,21 +6,21 @@ public class AuthenticationService {
 
     private static AuthenticationService INSTANCE = new AuthenticationService();
 
-    public static AuthenticationService getINSTANCE() {
+    public static AuthenticationService getInstance() {
         return INSTANCE;
     }
 
     private UserService userService = UserServiceFactory.getUserService();
 
-    public String getLoginUrl(final String url) {
+    public String getLoginURL(final String url) {
         return userService.createLoginURL(url);
     }
 
-    public String getLogoutUrl(final String url) {
+    public String getLogoutURL(final String url) {
         return userService.createLogoutURL(url);
     }
 
-    public Boolean isUserAdmin() {
+    public Boolean isAdmin() {
         return userService.isUserAdmin();
     }
 
