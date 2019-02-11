03e0855fc16e1962d7acc115d3322f539a931157
snipzer <l.arif@laposte.net>
2019-02-11 14:00:25 +0100

    refacto

--- src/main/java/com/zenika/zencontact/resource/UserResource.java
+++ src/main/java/com/zenika/zencontact/resource/UserResource.java
@@ -7,6 +7,8 @@ import com.zenika.zencontact.persistence.UserRepository;
 import com.zenika.zencontact.persistence.memcache.CacheService;
 import com.zenika.zencontact.persistence.objectify.UserDaoObjectify;
 import com.google.gson.Gson;
+import com.zenika.zencontact.util.StringUtil;
+
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
@@ -18,29 +20,26 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "UserResource", value = "/api/v0/users")
 public class UserResource extends HttpServlet {
 
-  public static String CONTACTS_CACHE_KEY = "contacts_cache";
-
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
-    List<User> contacts = (List<User>) CacheService.getINSTANCE().get(CONTACTS_CACHE_KEY);
-    System.out.println(contacts == null);
+    List<User> contacts = (List<User>) CacheService.getINSTANCE().get(StringUtil.CONTACTS_CACHE_KEY);
     if (contacts == null) {
       contacts = UserDaoObjectify.getInstance().getAll();
-      CacheService.getINSTANCE().put(CONTACTS_CACHE_KEY, contacts, Expiration.byDeltaSeconds(240),
+      CacheService.getINSTANCE().put(StringUtil.CONTACTS_CACHE_KEY, contacts, Expiration.byDeltaSeconds(240),
               MemcacheService.SetPolicy. ADD_ONLY_IF_NOT_PRESENT);
     }
-    response.setContentType("application/json; charset=utf-8");
+    response.setContentType(StringUtil.APPLICATION_JSON_CHARSET_UTF_8);
     response.getWriter().println(new Gson().toJsonTree(contacts).getAsJsonArray());
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
-    CacheService.getINSTANCE().delete(CONTACTS_CACHE_KEY);
+    CacheService.getINSTANCE().delete(StringUtil.CONTACTS_CACHE_KEY);
     User user = new Gson().fromJson(request.getReader(), User.class);
     user.id(UserDaoObjectify.getInstance().save(user));
-    response.setContentType("application/json; charset=utf-8");
+    response.setContentType(StringUtil.APPLICATION_JSON_CHARSET_UTF_8);
     response.setStatus(201);
     response.getWriter().println(new Gson().toJson(user));
   }
---
+++ src/main/java/com/zenika/zencontact/util/StringUtil.java
@@ -0,0 +1,6 @@
+package com.zenika.zencontact.util;
+
+public class StringUtil {
+    public static String CONTACTS_CACHE_KEY = "contacts_cache";
+    public static final String APPLICATION_JSON_CHARSET_UTF_8 = "application/json; charset=utf-8";
+}
